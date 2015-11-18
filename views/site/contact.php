7360ce342f49e13f98ac101205e31482804e335c
sdram58 <sdram58@hotmail.com>
2015-11-18 21:39:04 +0100

    Modificando cosas

--- views/site/contact.php
+++ views/site/contact.php
@@ -8,7 +8,7 @@ use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 use yii\captcha\Captcha;
 
-$this->title = 'Contact';
+$this->title = 'Contacto';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-contact">
@@ -49,7 +49,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
                     <?= $form->field($model, 'subject') ?>
 
-                    <?= $form->field($model, 'body')->textArea(['rows' => 6]) ?>
+                    <?= $form->field($model, 'Cuerpo')->textArea(['rows' => 6]) ?>
 
                     <?= $form->field($model, 'verifyCode')->widget(Captcha::className(), [
                         'template' => '<div class="row"><div class="col-lg-3">{image}</div><div class="col-lg-6">{input}</div></div>',
