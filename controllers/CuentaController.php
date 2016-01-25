45367e540db9f256cff63b29a74d7b947330ddee
sdram58 <sdram58@hotmail.com>
2016-01-25 21:48:21 +0100

    controllers20160125

--- controllers/CuentaController.php
+++ controllers/CuentaController.php
@@ -8,7 +8,7 @@ use app\models\CuentaSearch;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
-use kartik\mpdf\Pdf;
+use mPDF;
 
 
 /**
@@ -152,41 +152,23 @@ class CuentaController extends Controller
                 'model' => $model,'movimientos'=>$movimientos,'mpdf'=>$mpdf,
             ]);*/
             
-            /****************************************************************************************/
+            /*****************************************************************************************/
             $this->layout = 'printlayout';
+            $pdf = new mPDF('utf-8','A4-L',0,'',10,10,10,10);
             $movimientos = $model->getListado(Yii::$app->request->post('consulta'));  
             $content = $this->render('imprimir', [
-                'model' => $model,'movimientos'=>$movimientos,
+                'model' => $model,'movimientos'=>$movimientos,'mpdf'=>$pdf,
             ]);
- 
-            // setup kartik\mpdf\Pdf component
-            $pdf = new Pdf([
-                // set to use core fonts only
-                'mode' => Pdf::, 
-                // A4 paper format
-                'format' => Pdf::FORMAT_A4, 
-                // portrait orientation
-                'orientation' => Pdf::ORIENT_LANDSCAPE, 
-                // stream to browser inline
-                'destination' => Pdf::DEST_BROWSER, 
-                // your html content input
-                'content' => $content,  
-                // format content from your own css file if needed or use the
-                // enhanced bootstrap css built by Krajee for mPDF formatting 
-                'cssFile' => '@vendor/kartik-v/yii2-mpdf/assets/kv-mpdf-bootstrap.min.css',
-                // any css to be embedded if required
-                'cssInline' => '.kv-heading-1{font-size:18px}', 
-                 // set mPDF properties on the fly
-                'options' => ['title' => 'Krajee Report Title'],
-                 // call mPDF methods on the fly
-                'methods' => [ 
-                    'SetHeader'=>['Krajee Report Header'], 
-                    'SetFooter'=>['{PAGENO}'],
-                ]
-            ]);
+            
 
-            // return the pdf output as per the destination setting
-            return $pdf->render(); 
+ 
+  
+            
+            $pdf->WriteHTML($content);
+            $pdf->Output();
+            exit;
+   
+  
             
             /****************************************************************************************/
  
